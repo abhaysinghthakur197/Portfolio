3aa1dddfa439f7360e175cdb0be7c331ca75329d
Abhay Singh <59767465+abhaysinghthakur197@users.noreply.github.com>
2024-02-18 18:17:14 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,6 @@ import { Button, Container } from 'react-bootstrap';
 
 const About = () => {
   return (
-<<<<<<< HEAD
     <Container>
       <div
         name="about"
@@ -17,18 +16,6 @@ const About = () => {
               <div className="sm:text-right pb-8 pl-4">
                 <p className="text-2xl sm:text-4xl font-bold inline border-b-4 border-cyan-500 text-center ">
                   About
-=======
-    <div
-      name="about"
-      id="about"
-      className="w-full h-screen bg-[#0a192f] text-gray-300 pt-md-4 pt-lg-5 pt-xl-5 pt-9"
-    >
-      <div className="about flex flex-col justify-center items-center w-full h-full">
-        <div className=" py-16 rounded-md bg-cyan-800 flex flex-col justify-center items-center w-4/6">
-          <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8 mb-4">
-            <div className="sm:text-right pb-8 pl-4">
-              <p className="text-4xl font-bold inline border-b-4 border-cyan-500">
-                About
               </p>
             </div>
 
@@ -52,33 +39,12 @@ const About = () => {
                 In my free time, I enjoy sharing my insights and experiences through blogging about the latest trends and technologies in the industry.
                  <br /> <br />
                  Click on the resume button to get my latest resume.
->>>>>>> 29268f5beb52e37c17c73832e6524341ab276409
                 </p>
               </div>
 
             </div>
-            <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
-              <div className="sm:text-right text-lg sm:text-4xl font-bold">
-                <p className='max-w-md'>
-                  Welcome!
-                  <br /> It's a pleasure to connect with you.
-                </p>
-              </div>
-              <div>
-                <p className='sm:py-3 max-w-md'>
-                  {""}
-                  I am AWS Fullstack Developer with 2 years of hands-
-                  on expertise in React, Express, MongoDB, and Node.js.
-                  Proficient in AWS Lambda, DynamoDB, RDS, and API Gateway
-                  for cloud-based solutions. I have also worked with AWS security
-                  teams to implement secure application in AWS enviornment using AWS custom and managed config rule service.
-                  <br /> <br />
-                  In my free time I enjoy sharing my insights and experiences through blogging about the latest trends and technologies in the industry.
-                  <br /> <br />
-                  Click on the resume buttton to get my latests resume.
-                </p>
-              </div>
-            </div>
+            
+            
 
           </div>
           <div className='flex justify-center my-5'>
